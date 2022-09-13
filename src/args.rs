6df142a93d2d143b17f58797b2db8f68403e9258
qbibubi <58397100+qbibubi@users.noreply.github.com>
2022-09-13 16:21:04 +0200

    blueprnt working

--- src/args.rs
+++ src/args.rs
@@ -1,10 +1,29 @@
 use clap::{Args, Parser, Subcommand};
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
-#[clap(author, version, about)]
-pub struct DstrArgs {
+#[clap(author, version, about, long_about = None)]
+pub struct Cli {
     /// Project name
-    pub project_arg: String,
-    /// Project language
-    pub project_lang: String,
+    #[clap(value_parser)]
+    pub project_name: Option<String>,
+
+    /// Set custom config file
+    #[clap(short, long, value_parser, value_name = "FILE")]
+    pub project_config: Option<PathBuf>,
+
+    /// Enable debug information
+    #[clap(short, long, action = clap::ArgAction::Count)]
+    pub debug: u8,
+
+    #[clap(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Test {
+        #[clap(short, long, action)]
+        list: bool,
+    },
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,35 @@
 
 mod args;
 
-use args::DstrArgs;
+use args::Cli;
+use args::Commands;
 use clap::Parser;
 
 fn main() {
-    let _args = DstrArgs::parse();
+    let cli = Cli::parse();
+
+    if let Some(project_name) = cli.project_name.as_deref() {
+        println!("Project name: {}", project_name);
+    }
+
+    if let Some(config_path) = cli.project_config.as_deref() {
+        println!("Config: {}", config_path.display());
+    }
+
+    match cli.debug {
+        0 => println!("Debug OFF"),
+        1 => println!("Debug ON"),
+        _ => println!("It is ON. You can stop trying"),
+    }
+
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        None => {}
+    }
 }
